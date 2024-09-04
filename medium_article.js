2f284ca915f0a88771703c7c81916f886d5a71d5
ykunytskyi <ykunytskyi@perpet.io>
2024-09-04 17:30:26 +0300

    feat: add watch functionality for google calendar events

--- medium_article.js
+++ medium_article.js
@@ -1,44 +1,58 @@
-import { google } from "googleapis";
+import { calendar_v3, google } from "googleapis";
+import http from "http";
 
-console.log(">>> initting client");
 const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   "postmessage",
 );
-console.log(">>> client initted, gettings tokens");
 
 const { tokens } = await oAuth2Client.getToken(
   // code after granting consent on web
   "",
 );
-console.log(">>> got tokens:", tokens);
 
-console.log(">>> setting credentials");
 oAuth2Client.setCredentials({
   access_token: tokens.access_token,
   refresh_token: tokens.refresh_token,
 });
-console.log(">>> credentials set");
 
-console.log(">>> cerating calendar with oauth client");
 const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
-console.log(">>> calendar created");
-
-var now = new Date();
-var dateStr = now.toISOString().split("T")[0];
-
-// Set timeMin to start of the day and timeMax to end of the day in ISO format
-var timeMin = dateStr + "T00:00:00Z";
-var timeMax = dateStr + "T23:59:59Z";
-console.log(">>> listing events");
-const res = await calendar.events.list({
+calendar.events.watch({
   calendarId: "primary",
-  timeMin,
-  timeMax,
-  maxResults: 10,
-  singleEvents: true,
-  orderBy: "startTime",
+  requestBody: {
+    id: "unique-channel-id-" + new Date().getTime(),
+    type: "web_hook",
+    address: "https://96ed-46-118-187-83.ngrok-free.app",
+    params: {
+      ttl: 120, // seconds to live
+    },
+  },
 });
-// now you have events in res.data.items
-console.log("res:", res.data.items);
+
+/** @param {calendar_v3.Calendar} calendar */
+async function getEventsList(calendar) {
+  let now = new Date();
+  let dateStr = now.toISOString().split("T")[0];
+  let timeMin = dateStr + "T00:00:00Z";
+  let timeMax = dateStr + "T23:59:59Z";
+  return calendar.events.list({
+    calendarId: "primary",
+    timeMin,
+    timeMax,
+    maxResults: 5,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+}
+
+http
+  .createServer(async function (_, res) {
+    // get new list after detecting change
+    const list = await getEventsList(calendar);
+    console.log("list:", list.data.items);
+
+    res.write("OK");
+    res.end();
+  })
+  .listen(8080);
